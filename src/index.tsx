868b80f43b49bad2ca04fd1d594657c9f3bd2ef0
Gustavo Procopio <58342763+gustprocopio@users.noreply.github.com>
2021-06-27 08:27:11 -0300

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -13,20 +13,20 @@ createServer({
       transactions: [
         {
           id: 1, 
-          title: 'Freelance de Website',
+          title: 'SAM Bauru',
           type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2021-02-12 09:00:00')
+          category: 'Bauru',
+          amount: 1676.52,
+          createdAt: new Date('2021-06-22 09:10:00')
         },
-        {
-          id: 2, 
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2021-02-14 11:00:00')
-        }
+        // {
+        //   id: 2, 
+        //   title: 'Aluguel',
+        //   type: 'withdraw',
+        //   category: 'Casa',
+        //   amount: 1100,
+        //   createdAt: new Date('2021-02-14 11:00:00')
+        // }
       ],
     })
   },
